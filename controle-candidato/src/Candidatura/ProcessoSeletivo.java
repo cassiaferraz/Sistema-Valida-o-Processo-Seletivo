2d979e9fda15a623c48a5ba3cb10ead15da2edaa
Cassia Ferraz <cassialeticia.n.f@outlook.com>
2025-02-24 09:31:10 -0300

    imprimitCandidatos

--- controle-candidato/src/Candidatura/ProcessoSeletivo.java
+++ controle-candidato/src/Candidatura/ProcessoSeletivo.java
@@ -1,12 +1,50 @@
 package Candidatura;
 import java.util.concurrent.ThreadLocalRandom;
-
+import java.util.Random;
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-        selecaoCandidatos();
-        // analisarCandidato(1900.0);
-        // analisarCandidato(2200.0);
-        // analisarCandidato(2000.0);
+        String [] candidatos = {"FELIPE ", "MARCIA ", "JULIA ", "PAULO ", "AUGUSTO " };
+        for(String candidato: candidatos) {
+       }
+    }
+
+    static void entrandoEmContato(String candidato){
+        int tentativasRealizadas = 1;
+        boolean continuarTentando =true;
+        boolean atendeu=false;
+        do {
+            atendeu= atender();
+            continuarTentando = !atendeu;
+            if(continuarTentando)
+              tentativasRealizadas++;
+              else 
+                  System.out.println("CONTATO REALIZADO COM SUCESSO");
+    
+        }while(continuarTentando && tentativasRealizadas<3);
+    
+        if(atendeu)
+            System.out.println("CONSEGUIMOS CONTATO COM " + candidato + " NA " + tentativasRealizadas + "TENTATIVA");
+
+        else
+            System.out.println("NÃO CONSEGUIMOS CONTANTO COM " + candidato + "NÚMERO MÁXIMO DE TENTATIVAS " + tentativasRealizadas + "REALIZADA" );
+    
+    }
+
+
+    //metodo auxiliar
+    static boolean atender() {
+    return new Random().nextInt(3)==1;
+    }
+
+
+
+    static void imprimirCandidatos(){
+        String [] candidatos = {"FELIPE ", "MARCIA ", "JULIA ", "PAULO ", "AUGUSTO " };
+    System.out.println("Imprimindo a lista de candidatos infrmando o indice do elemento");
+
+        for(int indice=0; indice < candidatos.length;indice++){
+            System.out.println("O candidato de nº " + (indice+1) + " é o " + candidatos[indice]);
+        }
     }
 
     static void selecaoCandidatos(){
